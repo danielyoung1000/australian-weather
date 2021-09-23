5816e4d5087ff54b6710658d8206f913cac313c8
Daniel Young <daniel.young1@nhs.net>
2021-09-23 11:38:42 +0100

    Create timeseries

--- R/create-plot.R
+++ R/create-plot.R
@@ -18,6 +18,14 @@ scatter_plot =
   geom_point(colour = "orange") +
   ggtitle(paste("Plot of ", city, sep = ""))
 
+time_series =
+  df %>%
+  filter(Location == city) %>%
+  ggplot(aes(x = Date, y = Temp9am)) +
+  geom_line()
+ggsave(filename = "graphics/timeseries-rainfall.png",
+       plot = time_series)
+
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
 
