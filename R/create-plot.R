6d4605852168b4d991a04d3a2bd66c978758a8df
Daniel Young <daniel.young1@nhs.net>
2021-09-23 09:56:39 +0100

    Chart title now based on city variable

--- R/create-plot.R
+++ R/create-plot.R
@@ -9,14 +9,14 @@ df = read_csv("data/weatherAUS.csv")
 # Create a scatter plot of Humidity9am and Temp9am
 # a location
 # test 24
-city = "Sydney"
+city = "Brisbane"
 
 scatter_plot =
   df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) +
   geom_point(colour = "orange") +
-  ggtitle("Plot of Sydney")
+  ggtitle(paste("Plot of ", city, sep = ""))
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
